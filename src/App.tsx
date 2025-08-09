4bfe2cfa65893b1ff554a91d647a391195247d47
Lucas Ribeiro <lucasribeirollr03@gmail.com>
2025-08-09 02:34:14 -0300

    menu component

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import "./styles/global.css";
 import Container from "./components/Container";
 import Heading from "./components/Heading";
 import Logo from "./components/Logo";
+import Menu from "./components/Menu";
 
 function App() {
   return (
@@ -11,7 +12,7 @@ function App() {
         <Logo />
       </Container>
       <Container>
-        <Heading>LOGO</Heading>
+        <Menu />
       </Container>
       <Container>
         <Heading>LOGO</Heading>
