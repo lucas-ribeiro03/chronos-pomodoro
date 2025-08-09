7d16b3e26b5dff96be658b35d5b571978c094fb6
Lucas Ribeiro <lucasribeirollr03@gmail.com>
2025-08-09 02:39:11 -0300

    countdown component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import "./styles/theme.css";
 import "./styles/global.css";
 import Container from "./components/Container";
-import Heading from "./components/Heading";
 import Logo from "./components/Logo";
 import Menu from "./components/Menu";
+import Countdown from "./components/Countdown";
 
 function App() {
   return (
@@ -15,7 +15,7 @@ function App() {
         <Menu />
       </Container>
       <Container>
-        <Heading>LOGO</Heading>
+        <Countdown />
       </Container>
     </>
   );
