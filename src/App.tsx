26f2ed3af8fb5600b68abff97ecdccb9b6e50563
Lucas Ribeiro <lucasribeirollr03@gmail.com>
2025-08-11 03:30:41 -0300

    rendering home component only

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,9 @@
 import "./styles/theme.css";
 import "./styles/global.css";
-import Container from "./components/Container";
-import Logo from "./components/Logo";
-import Menu from "./components/Menu";
-import Countdown from "./components/Countdown";
-import Input from "./components/Input";
-import Footer from "./components/Footer";
+import Home from "./Pages/Home";
 
 function App() {
-  return (
-    <>
-      <Container>
-        <Logo />
-      </Container>
-      <Container>
-        <Menu />
-      </Container>
-      <Container>
-        <Countdown />
-      </Container>
-      <Container>
-        <form className="form">
-          <div className="formRow">
-            <Input
-              id="input"
-              type="text"
-              labelText="task"
-              placeholder="Digite a tarefa"
-            />
-          </div>
-          <div className="formRow">
-            <span>
-              Nesse ciclo <strong>foque</strong>por <strong>25 min.</strong>
-            </span>
-          </div>
-          <div className="formRow">
-            <button>botao</button>
-          </div>
-        </form>
-      </Container>
-      <Container>
-        <Footer />
-      </Container>
-    </>
-  );
+  return <Home />;
 }
 
 export default App;
