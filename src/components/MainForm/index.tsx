3a6f51e22c9ebea6b2a46569ac5d038d659b2adc
Lucas Ribeiro <lucasribeirollr03@gmail.com>
2025-08-12 02:09:36 -0300

    function to add a task

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -1,17 +1,55 @@
 import { useRef } from "react";
 import Input from "../Input";
+import { useTaskContext } from "../../context/TaskContext/useTaskContext";
+import type { TaskModel } from "../../models/TaskModel";
 
 const MainForm = () => {
-  const inputRef = useRef<HTMLInputElement>(null);
+  const taskNameInput = useRef<HTMLInputElement>(null);
+
+  const { setState } = useTaskContext();
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (!taskNameInput.current) return alert("Insira uma tarefa");
+    const taskName = taskNameInput.current.value.trim();
+
+    if (!taskName) return alert("Insira uma tarefa");
+
+    const newTask: TaskModel = {
+      id: Date.now().toString(),
+      name: taskName,
+      duration: 1,
+      startDate: Date.now(),
+      completeDate: null,
+      interruptDate: null,
+      type: "workTime",
+    };
+
+    const secondsRemaining = newTask.duration * 60;
+
+    setState((prev) => {
+      return {
+        ...prev,
+        activeTask: newTask,
+        currentCycle: 1,
+        secondsRemaining,
+        formattedSecondsRemaining: "00:00",
+        tasks: [...prev.tasks, newTask],
+        config: { ...prev.config },
+      };
+    });
+  };
+
   return (
-    <form className="form">
+    <form onSubmit={onSubmit} className="form">
       <div className="formRow">
         <Input
           id="input"
           type="text"
           labelText="task"
           placeholder="Digite a tarefa"
-          ref={inputRef}
+          ref={taskNameInput}
         />
       </div>
       <div className="formRow">
