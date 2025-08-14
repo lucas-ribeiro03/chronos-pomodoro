86266645806f657765062e0aeedb78d8f7909965
Lucas Ribeiro <lucasribeirollr03@gmail.com>
2025-08-14 07:57:26 -0300

    task actions

---
+++ src/context/taskActions.ts
@@ -0,0 +1,25 @@
+import type { TaskModel } from "../models/TaskModel";
+
+export enum TaskActionTypes {
+  START_TASK = "START_TASK",
+  INTERRUPT_TASK = "INTERRUPT_TASK",
+  RESET_TASK = "RESET_TASK",
+}
+
+export type TaskActionModelWithPayload =
+  | {
+      type: TaskActionTypes.START_TASK;
+      payload: TaskModel;
+    }
+  | {
+      type: TaskActionTypes.INTERRUPT_TASK;
+      payload: TaskModel;
+    };
+
+export type TaskActionModelWithoutPayload = {
+  type: TaskActionTypes.RESET_TASK;
+};
+
+export type TaskActionModel =
+  | TaskActionModelWithPayload
+  | TaskActionModelWithoutPayload;
