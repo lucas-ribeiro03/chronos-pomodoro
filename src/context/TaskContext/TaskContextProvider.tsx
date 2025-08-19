77ff10ce5773012cdc7a5d7f5ca8654ca611ee3a
Lucas Ribeiro <lucasribeirollr03@gmail.com>
2025-08-19 11:16:36 -0300

    actions with workers

--- src/context/TaskContext/TaskContextProvider.tsx
+++ src/context/TaskContext/TaskContextProvider.tsx
@@ -1,7 +1,9 @@
-import { useReducer } from "react";
+import { useEffect, useReducer } from "react";
 import { initialState } from "./initialTaskState";
 import { TaskContext } from "./TaskContext";
 import { taskReducer } from "./taskReducer";
+import { TimerWorkerManager } from "../../workers/timerWorkerManager";
+import { TaskActionTypes } from "./taskActions";
 
 type TaskContextProviderProps = {
   children: React.ReactNode;
@@ -9,6 +11,33 @@ type TaskContextProviderProps = {
 
 export const TaskContextProvider = ({ children }: TaskContextProviderProps) => {
   const [state, dispatch] = useReducer(taskReducer, initialState);
+  const worker = TimerWorkerManager.getInstance();
+
+  worker.onmessage((e) => {
+    const countDownSeconds = e.data;
+
+    if (countDownSeconds <= 0) {
+      dispatch({
+        type: TaskActionTypes.COMPLETE_TASK,
+      });
+      worker.terminate();
+    } else {
+      dispatch({
+        type: TaskActionTypes.COUNT_DOWN,
+        payload: { secondsRemaining: countDownSeconds },
+      });
+    }
+  });
+
+  useEffect(() => {
+    console.log(state);
+    if (!state.activeTask) {
+      worker.terminate();
+      console.log("sem active task");
+    }
+
+    worker.postMessage(state);
+  }, [state, worker]);
   return (
     <TaskContext.Provider value={{ state, dispatch }}>
       {children}
