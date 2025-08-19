61a07adb3b9bb3acadf2cf32c142f12d7473e676
Lucas Ribeiro <lucasribeirollr03@gmail.com>
2025-08-19 07:31:38 -0300

    countdown and complete task

--- src/context/TaskContext/taskActions.ts
+++ src/context/TaskContext/taskActions.ts
@@ -4,12 +4,19 @@ export enum TaskActionTypes {
   START_TASK = "START_TASK",
   INTERRUPT_TASK = "INTERRUPT_TASK",
   RESET_TASK = "RESET_TASK",
+  COUNT_DOWN = "COUNT_DOWN",
+  COMPLETE_TASK = "COMPLETE_TASK",
 }
 
-export type TaskActionModelWithPayload = {
-  type: TaskActionTypes.START_TASK;
-  payload: TaskModel;
-};
+export type TaskActionModelWithPayload =
+  | {
+      type: TaskActionTypes.START_TASK;
+      payload: TaskModel;
+    }
+  | {
+      type: TaskActionTypes.COUNT_DOWN;
+      payload: { secondsRemaining: number };
+    };
 
 export type TaskActionModelWithoutPayload =
   | {
@@ -17,6 +24,9 @@ export type TaskActionModelWithoutPayload =
     }
   | {
       type: TaskActionTypes.INTERRUPT_TASK;
+    }
+  | {
+      type: TaskActionTypes.COMPLETE_TASK;
     };
 
 export type TaskActionModel =
