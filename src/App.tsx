c8fe211e03dffd0cd3a7abcf303ecd796ebe0afe
Lucas Ribeiro <lucasribeirollr03@gmail.com>
2025-08-19 13:33:53 -0300

    react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import "./styles/theme.css";
 import "./styles/global.css";
-import Home from "./Pages/Home";
 import { TaskContextProvider } from "./context/TaskContext/TaskContextProvider";
 import MessageContainer from "./components/MessageContainer";
+import { Router } from "./router/router";
 
 function App() {
   return (
     <TaskContextProvider>
       <MessageContainer>
-        <Home />;
+        <Router />
       </MessageContainer>
     </TaskContextProvider>
   );
--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,7 +1,18 @@
-import { BrowserRouter, Route, Routes } from "react-router";
+import { BrowserRouter, Route, Routes, useLocation } from "react-router";
 import Home from "../Pages/Home";
 import { AboutPomodoro } from "../Pages/About";
 import { NotFound } from "../Pages/NotFound";
+import { useEffect } from "react";
+
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  }, [pathname]);
+
+  return null;
+};
 
 export const Router = () => {
   return (
@@ -11,6 +22,7 @@ export const Router = () => {
         <Route path="/about-pomodoro" element={<AboutPomodoro />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
+      <ScrollToTop />
     </BrowserRouter>
   );
 };
